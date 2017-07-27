76a640fef56f68c6b1a9fa8d597d3d7571e2b028
German Schnyder <gschnyder@gmail.com>
2017-07-27 20:30:04 -0300

    Updating azure retry file

--- azure_retry_failed.py
+++ azure_retry_failed.py
@@ -32,6 +32,8 @@ import time
 import configparser
 import logging
 
+from azure.batch.models import TaskListOptions
+
 from common import helpers
 from common.instruments import ACS, WFC3, STIS, WFPC2, NICMOS
 
@@ -75,40 +77,10 @@ _NODE_OS_SKU = config.get(option='sku', section='Node')
 _CONTAINER = config.get(option='container', section='Job')
 _FOLDER = config.get(option='folder', section='Job')
 
-_JOB_ID = config.get(option='prefix', section='Job') + '_' + str(_FOLDER).replace('/', '_').replace('.', '_')
+_JOB_ID = 'HubbleImagesJob_home10_german_schnyder_disco3_'
 
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-
-
-def query_yes_no(question, default="yes"):
-    """
-    Prompts the user for yes/no input, displaying the specified question text.
-
-    :param str question: The text of the prompt for input.
-    :param str default: The default if the user hits <ENTER>. Acceptable values
-    are 'yes', 'no', and None.
-    :rtype: str
-    :return: 'yes' or 'no'
-    """
-    valid = {'y': 'yes', 'n': 'no'}
-    if default is None:
-        prompt = ' [y/n] '
-    elif default == 'yes':
-        prompt = ' [Y/n] '
-    elif default == 'no':
-        prompt = ' [y/N] '
-    else:
-        raise ValueError("Invalid default answer: '{}'".format(default))
-
-    while 1:
-        choice = input(question + prompt).lower()
-        if default and not choice:
-            return default
-        try:
-            return valid[choice[0]]
-        except (KeyError, IndexError):
-            print("Please respond with 'yes' or 'no' (or 'y' or 'n').\n")
 
 
 def print_batch_exception(batch_exception):
@@ -155,7 +127,7 @@ def upload_file_to_container(block_blob_client, container_name, folder, file_pat
         container_name,
         blob_name,
         permission=azureblob.BlobPermissions.READ,
-        expiry=datetime.datetime.utcnow() + datetime.timedelta(hours=48))
+        expiry=datetime.datetime.utcnow() + datetime.timedelta(hours=480))
 
     sas_url = block_blob_client.make_blob_url(container_name,
                                               blob_name,
@@ -173,7 +145,7 @@ def get_file_details(block_blob_client, container_name, file_name):
         container_name,
         file_name,
         permission=azureblob.BlobPermissions.READ,
-        expiry=datetime.datetime.utcnow() + datetime.timedelta(hours=48))
+        expiry=datetime.datetime.utcnow() + datetime.timedelta(hours=480))
 
     sas_url = block_blob_client.make_blob_url(container_name,
                                               file_name,
@@ -190,7 +162,7 @@ def get_file_details(block_blob_client, container_name, file_name):
         container_name,
         pos_file_name,
         permission=azureblob.BlobPermissions.READ,
-        expiry=datetime.datetime.utcnow() + datetime.timedelta(hours=48))
+        expiry=datetime.datetime.utcnow() + datetime.timedelta(hours=480))
 
     pos_sas_url = block_blob_client.make_blob_url(container_name,
                                                   pos_file_name,
@@ -224,86 +196,25 @@ def get_container_sas_token(block_blob_client,
         block_blob_client.generate_container_shared_access_signature(
             container_name,
             permission=blob_permissions,
-            expiry=datetime.datetime.utcnow() + datetime.timedelta(hours=48))
+            expiry=datetime.datetime.utcnow() + datetime.timedelta(hours=480))
 
     return container_sas_token
 
 
-def create_pool(batch_service_client, pool_id,
-                resource_files, publisher, offer, sku):
-    """
-    Creates a pool of compute nodes with the specified OS settings.
+def list_unfinished_job_tasks(batch_service_client, job_id, max_results):
 
-    :param batch_service_client: A Batch service client.
-    :type batch_service_client: `azure.batch.BatchServiceClient`
-    :param str pool_id: An ID for the new pool.
-    :param list resource_files: A collection of resource files for the pool's
-    start task.
-    :param str publisher: Marketplace image publisher
-    :param str offer: Marketplace image offer
-    :param str sku: Marketplace image sku
-    """
-    try:
-        pool = batch_service_client.pool.get(pool_id=_POOL_ID)
-        logging.info('Pool already exists [{}]... nothing done.'.format(pool_id))
+    tasks = batch_service_client.task.list(job_id, task_list_options=TaskListOptions(
+        max_results=max_results,
+        filter='state eq \'active\''
+    ))
 
-    except Exception:
+    incomplete_tasks = [task for task in tasks if
+                        task.state != batchmodels.TaskState.completed]
 
-        logging.info('Creating pool [{}]...'.format(pool_id))
+    for itask in incomplete_tasks:
+        logging.info('Got task [{}]...'.format(itask))
 
-        # Create a new pool of Linux compute nodes using an Azure Virtual Machines
-        # Marketplace image. For more information about creating pools of Linux
-        # nodes, see:
-        # https://azure.microsoft.com/documentation/articles/batch-linux-nodes/
-
-        # Specify the commands for the pool's start task. The start task is run
-        # on each node as it joins the pool, and when it's rebooted or re-imaged.
-        # We use the start task to prep the node for running our task script.
-        task_commands = [
-            # Copy the application scripts to the "shared" directory
-            # that all tasks that run on the node have access to.
-            'cp -r $AZ_BATCH_TASK_WORKING_DIR/* $AZ_BATCH_NODE_SHARED_DIR',
-            'chmod 777 -R $AZ_BATCH_NODE_SHARED_DIR',
-            # Install pip and the dependencies for cryptography
-            'apt-get update',
-            'apt-get -y install python3-pip',
-            'pip3 install --upgrade pip',
-            'apt-get -y install build-essential libssl-dev libffi-dev python-dev',
-            # Install the azure-storage module so that the task script can access
-            # Azure Blob storage
-            'pip3 install azure-storage azure-batch',
-            'pip3 install --upgrade numpy scipy astropy argparse scikit-image matplotlib'
-        ]
-
-        # Get the node agent SKU and image reference for the virtual machine
-        # configuration.
-        # For more information about the virtual machine configuration, see:
-        # https://azure.microsoft.com/documentation/articles/batch-linux-nodes/
-        sku_to_use, image_ref_to_use = \
-            common.helpers.select_latest_verified_vm_image_with_node_agent_sku(
-                batch_service_client, publisher, offer, sku)
-
-        new_pool = batch.models.PoolAddParameter(
-            id=pool_id,
-            virtual_machine_configuration=batchmodels.VirtualMachineConfiguration(
-                image_reference=image_ref_to_use,
-                node_agent_sku_id=sku_to_use),
-            vm_size=_POOL_VM_SIZE,
-            max_tasks_per_node=2,
-            target_dedicated=_POOL_NODE_COUNT,
-            start_task=batch.models.StartTask(
-                command_line=common.helpers.wrap_commands_in_shell('linux',
-                                                                   task_commands),
-                run_elevated=True,
-                wait_for_success=True,
-                resource_files=resource_files),
-        )
-
-        try:
-            batch_service_client.pool.add(new_pool)
-        except batchmodels.batch_error.BatchErrorException as err:
-            print_batch_exception(err)
-            raise
+    return incomplete_tasks
 
 
 def create_job(batch_service_client, job_id, pool_id):
@@ -379,69 +290,6 @@ def add_tasks(batch_service_client, job_id, input_files,
     logging.info("Just dispatched last batch of %d" % len(tasks))
 
 
-def wait_for_tasks_to_complete(batch_service_client, job_id, timeout):
-    """
-    Returns when all tasks in the specified job reach the Completed state.
-
-    :param batch_service_client: A Batch service client.
-    :type batch_service_client: `azure.batch.BatchServiceClient`
-    :param str job_id: The id of the job whose tasks should be to monitored.
-    :param timedelta timeout: The duration to wait for task completion. If all
-    tasks in the specified job do not reach Completed state within this time
-    period, an exception will be raised.
-    """
-    timeout_expiration = datetime.datetime.now() + timeout
-
-    print("Monitoring all tasks for 'Completed' state, timeout in {}..."
-          .format(timeout), end='')
-
-    while datetime.datetime.now() < timeout_expiration:
-        print(' o ', end='')
-        sys.stdout.flush()
-        tasks = batch_service_client.task.list(job_id)
-
-        incomplete_tasks = [task for task in tasks if
-                            task.state != batchmodels.TaskState.completed]
-        if not incomplete_tasks:
-            print()
-            return True
-        else:
-            time.sleep(3600)
-
-    print()
-    raise RuntimeError("ERROR: Tasks did not reach 'Completed' state within "
-                       "timeout period of " + str(timeout))
-
-
-def download_blobs_from_container(block_blob_client, container_name, directory_path):
-    """
-    Downloads all blobs from the specified Azure Blob storage container.
-
-    :param block_blob_client: A blob service client.
-    :type block_blob_client: `azure.storage.blob.BlockBlobService`
-    :param container_name: The Azure Blob storage container from which to
-     download files.
-    :param directory_path: The local directory to which to download the files.
-    """
-    logging.info('Downloading all files from container [{}]...'.format(container_name))
-
-    container_blobs = block_blob_client.list_blobs(container_name)
-
-    for blob in container_blobs.items:
-        destination_file_path = os.path.join(directory_path, blob.name)
-
-        block_blob_client.get_blob_to_path(container_name,
-                                           blob.name,
-                                           destination_file_path)
-
-        logging.debug('Downloaded blob [{}] from container [{}] to {}'.format(
-            blob.name,
-            container_name,
-            destination_file_path))
-
-    logging.debug('Download complete!')
-
-
 def is_valid_extension(blob):
     valid_extensions_arr = [ACS.DATA_FILE_EXT, WFC3.DATA_FILE_EXT, STIS.DATA_FILE_EXT, WFPC2.DATA_FILE_EXT,
                             NICMOS.DATA_FILE_EXT]
@@ -455,74 +303,35 @@ def is_valid_extension(blob):
 
 if __name__ == '__main__':
 
-    start_time = datetime.datetime.now().replace(microsecond=0)
-    logging.info('Sample start: {}'.format(start_time))
+    app_container_name = 'application'
+    output_container_name = 'output'
 
-    # Create the blob client, for use in obtaining references to
-    # blob storage containers and uploading files to containers.
     blob_client = azureblob.BlockBlobService(
         account_name=_STORAGE_ACCOUNT_NAME,
         account_key=_STORAGE_ACCOUNT_KEY)
 
-    # Use the blob client to create the containers in Azure Storage if they
-    # don't yet exist.
-    app_container_name = 'application'
-    output_container_name = 'output'
-    blob_client.create_container(app_container_name, fail_on_exist=False)
-    blob_client.create_container(output_container_name, fail_on_exist=False)
-
-    # Paths to the task script. This script will be executed by the tasks that
-    # run on the compute nodes.
-    application_file_paths = {
-        os.path.realpath('common/image.py'): 'common',
-        os.path.realpath('common/helpers.py'): 'common',
-        os.path.realpath('common/instruments.py'): 'common',
-        os.path.realpath('db/azure.py'): 'db',
-        os.path.realpath('lib/crutils.py'): 'lib',
-        os.path.realpath('lib/calc_pos.py'): 'lib',
-        os.path.realpath('external/cosmics.py'): 'external',
-        os.path.realpath('requirements.txt'): '',
-        os.path.realpath('configuration.cfg'): '',
-        os.path.realpath('azure_task.py'): ''
-    }
-
-    # Upload the application script to Azure Storage. This is the script that
-    # will process the data files, and is executed by each of the tasks on the
-    # compute nodes.
-    application_files = [
-        upload_file_to_container(blob_client, app_container_name, folder, file_path)
-        for file_path, folder in application_file_paths.items()
-    ]
-
-    # Lets just get some images for testing
     blobs = []
     marker = None
     container = _CONTAINER
     prefix = _FOLDER
 
-    while True:
-        bbatch = blob_client.list_blobs(container_name=container, prefix=prefix, marker=marker, delimiter='/')
-        blobs.extend(bbatch)
-        if not bbatch.next_marker:
-            break
-        marker = bbatch.next_marker
+    # while True:
+    #     bbatch = blob_client.list_blobs(container_name=container, prefix=prefix, marker=marker, delimiter='/')
+    #     blobs.extend(bbatch)
+    #     if not bbatch.next_marker:
+    #         break
+    #     marker = bbatch.next_marker
+    #
+    # input_files = [
+    #     get_file_details(blob_client, 'all', blob.name)
+    #     for blob in blobs if is_valid_extension(blob)
+    # ]
+    #
+    # output_container_sas_token = get_container_sas_token(
+    #     blob_client,
+    #     output_container_name,
+    #     azureblob.BlobPermissions.WRITE)
 
-    logging.info("Got %d images from %s/%s" % (len(blobs), container, prefix))
-
-    input_files = [
-        get_file_details(blob_client, 'all', blob.name)
-        for blob in blobs if is_valid_extension(blob)
-    ]
-
-    # Obtain a shared access signature that provides write access to the output
-    # container to which the tasks will upload their output.
-    output_container_sas_token = get_container_sas_token(
-        blob_client,
-        output_container_name,
-        azureblob.BlobPermissions.WRITE)
-
-    # Create a Batch service client. We'll now be interacting with the Batch
-    # service in addition to Storage
     credentials = batchauth.SharedKeyCredentials(_BATCH_ACCOUNT_NAME,
                                                  _BATCH_ACCOUNT_KEY)
 
@@ -530,61 +339,19 @@ if __name__ == '__main__':
         credentials,
         base_url=_BATCH_ACCOUNT_URL)
 
-    try:
+    unfinished_tasks = list_unfinished_job_tasks(batch_client, _JOB_ID, 10)
 
-        # Create the pool that will contain the compute nodes that will execute the
-        # tasks. The resource files we pass in are used for configuring the pool's
-        # start task, which is executed each time a node first joins the pool (or
-        # is rebooted or re-imaged).
-        create_pool(batch_client,
-                    _POOL_ID,
-                    application_files,
-                    _NODE_OS_PUBLISHER,
-                    _NODE_OS_OFFER,
-                    _NODE_OS_SKU)
+    print(unfinished_tasks[0])
 
-        # Create the job that will run the tasks.
-        create_job(batch_client, _JOB_ID, _POOL_ID)
-
-        # Add the tasks to the job. We need to supply a container shared access
-        # signature (SAS) token for the tasks so that they can upload their output
-        # to Azure Storage.
-        add_tasks(batch_client,
-                  _JOB_ID,
-                  input_files,
-                  output_container_name,
-                  output_container_sas_token)
-
-        # Pause execution until tasks reach Completed state.
-        # wait_for_tasks_to_complete(batch_client, _JOB_ID, datetime.timedelta(hours=48))
-
-        # logging.info(" Success! All tasks reached the 'Completed' state within the specified timeout period.")
-
-        # Download the task output files from the output Storage container to a
-        # local directory (only if i'm working with a small set)
-        # if len(blobs) < 150:
-        #    download_blobs_from_container(blob_client, output_container_name, os.path.expanduser('~/Downloads'))
-
-        # Clean up storage resources
-        # logging.info('Deleting containers...')
-        # blob_client.delete_container(app_container_name)
-        # blob_client.delete_container(output_container_name)
-
-        # Print out some timing info
-        # end_time = datetime.datetime.now().replace(microsecond=0)
-
-        # logging.info('Sample end: {}'.format(end_time))
-        # logging.info('Elapsed time: {}'.format(end_time - start_time))
-
-    except Exception as e:
-        logging.exception('Unexpected error')
-
-    # Clean up Batch resources (if the user so chooses).
-    # if query_yes_no('Delete job?') == 'yes':
-
-    # batch_client.job.delete(_JOB_ID)
-
-    # if query_yes_no('Delete pool?') == 'yes':
-    # batch_client.pool.delete(_POOL_ID)
-
-    # input('Press ENTER to exit... don\'t forget to delete pool and jobs')
+    # try:
+    #
+    #     create_job(batch_client, _JOB_ID, _POOL_ID)
+    #
+    #     add_tasks(batch_client,
+    #               _JOB_ID,
+    #               input_files,
+    #               output_container_name,
+    #               output_container_sas_token)
+    #
+    # except Exception as e:
+    #     logging.exception('Unexpected error')
